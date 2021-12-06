446b77a68cabb78f05b1ebeb6814a9fcd39d8d3c
Filipster <55165630+Filipster@users.noreply.github.com>
2021-12-06 19:56:36 +0300

    Add files via upload

---
+++ main-project/opencv_projeto/opencv_projeto/libraries_opencv.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2\opencv.hpp>
+
