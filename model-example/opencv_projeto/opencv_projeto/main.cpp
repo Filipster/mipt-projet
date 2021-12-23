d6c7b98cd5bbfd9c809315633de9f96d0e3d8ac1
Filipster <55165630+Filipster@users.noreply.github.com>
2021-12-23 16:22:45 -0300

    Update main.cpp

--- model-example/opencv_projeto/opencv_projeto/main.cpp
+++ model-example/opencv_projeto/opencv_projeto/main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "libraries_opencv.h"
+#include "class.h"
 
 using namespace cv;
-
 CascadeClassifier faceCascade;
 
-void faceDetection(Mat frame) {
-    double scale = 1.0;
-
-    Mat grayscale;
-    cvtColor(frame, grayscale, COLOR_BGR2GRAY);
-    resize(grayscale, grayscale, Size(grayscale.size().width / scale, grayscale.size().height / scale));
-
-    //face detection setup 
-    std::vector<Rect> faces;
-    faceCascade.detectMultiScale(grayscale, faces, 1.1, 3, 0, Size(30, 30));
-
-    for (Rect area : faces) {
-        Scalar drawColor = Scalar(255, 0, 0); 
-        //geometric-scalar recognizer setup
-        rectangle(frame, Point(cvRound(area.x * scale), cvRound(area.y * scale)),
-            Point(cvRound((area.x + area.width - 1) * scale), cvRound((area.y + area.height - 1) * scale)), drawColor);
-    }
-    imshow("detection", frame);
-}
 
 int main(int argc, const char** argv) {
     //file upload
@@ -40,18 +23,16 @@ int main(int argc, const char** argv) {
         return -1;
     //frame settings
     Mat frame;
+    faceDetector facedetector;
     while (cap.read(frame)) {
         if (frame.empty()) {
             std::cout << "No Frame";
             break;
         }
-        faceDetection(frame);
+        facedetector.face_detector(frame);
         if (waitKey(15) >= 0) {
             break;
         }
     }
     return 0;
 }
-
-
-
