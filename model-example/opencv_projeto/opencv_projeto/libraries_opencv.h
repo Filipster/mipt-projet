6962dc5a4d506f4ced49b142d9719907b8a90315
Filipster <55165630+Filipster@users.noreply.github.com>
2021-12-23 16:22:10 -0300

    Update libraries_opencv.h

--- model-example/opencv_projeto/opencv_projeto/libraries_opencv.h
+++ model-example/opencv_projeto/opencv_projeto/libraries_opencv.h
@@ -2,8 +2,8 @@
 
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include <opencv2\opencv.hpp>
+#include <vector>
+#include <string>
 
